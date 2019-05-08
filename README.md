5e2d69c9dae29a8c8d90ca080402af512064a200
Eugenio <eugeniodonaque@gmail.com>
2019-05-08 20:26:19 +0200

    Proyecto gps creado. Especificación de 'estoc' comenzada.

---
+++ README.md
@@ -0,0 +1,5 @@
+Este proyecto se corresponde con la Práctica evaluada 3 de la asignatura Estructura de Datos.
+
+Grado en Ingeniería informática en la Universidad de las Islas Baleares.
+
+Curso 2018-19
---
+++ src/destoc.ads
@@ -0,0 +1,27 @@
+package destoc is
+   type marca is (Nike, Adidas, Reebok, Asics,Fila, Puma, Quiksilver, Kappa,
+                  Joma, Convers);
+   type codi is new Natural range 0..5000000;
+   type nom is new String(1..33);
+   type estoc is limited private;
+
+   procedure estoc_buit(c: out estoc);
+   procedure posar_producte(c: in out estoc; m: in marca; k: in codi;
+                            n: in nom; unitats: in integer);
+   procedure esborrar_producte(c: in out estoc; k: in codi);
+   procedure imprimir_productes_marca(c: in estoc; m: in marca);
+   procedure imprimir_estoc_total(c: in estoc);
+private
+
+   type producte is record
+      n: nom;
+      m: marca;
+      c: codi;
+      u: integer;
+      end record;
+
+   type estoc is record
+      p: integer;
+   end record;
+
+end destoc;
